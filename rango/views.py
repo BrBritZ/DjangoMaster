006cb2fed3589da9fc0ab199159fd00811ca3c5c
BrBritZ <brbritz156@gmail.com>
2017-01-17 22:17:17 +0000

    [Chapter6], Exercises part-view count for page and top view/like list

--- rango/views.py
+++ rango/views.py
@@ -13,7 +13,8 @@ def index(request):
     # That will be passed to the template engine.
 
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 
     # Render the response and sent it back!
     return render(request, 'rango/index.html', context_dict)
